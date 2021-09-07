5f76fa2ceb77bbd3daf2edff006e85ae559c4919
Ashish Papanai <ashishpapanai00@gmail.com>
2021-09-07 22:57:50 +0530

    Fixes

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from numpy.lib import utils
+from lane import Lane
+import cv2
+import time
+import pyttsx3 as p
+from utils import Util
+
+if __name__ == "__main__":
+    video = cv2.VideoCapture(r'E:\road-vision-ai\data\data.mp4') # replace with the full path of video
+    lane = Lane(video)
+    lane.run(video)
+    util = Util()
+    util.speak()
+
+    
