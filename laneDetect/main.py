39019240040876d514f1ea7d6c1555cf17f0a4a2
Ashish Papanai <ashishpapanai00@gmail.com>
2021-09-18 18:26:41 +0530

    Added: Output images

--- laneDetect/main.py
+++ laneDetect/main.py
@@ -6,7 +6,7 @@ import pyttsx3 as p
 from utils import Util
 
 if __name__ == "__main__":
-    video_path = r'E:\road-vision-ai\data\data.mp4' # replace with the full path of video
+    video_path = r'E:\road-vision-ai\data\test_vid.mp4' # replace with the full path of video
     lane = Lane(video_path)
     lane.run(path=video_path)
     util = Util()
