4d4724e80e1a907014fd49b28a3e7e4975831f7a
Ashish Papanai <ashishpapanai00@gmail.com>
2021-09-27 19:20:08 +0530

    Added: Images

--- objDetect/main.py
+++ objDetect/main.py
@@ -3,7 +3,7 @@ from objUtils import *
 
 
 if __name__ == '__main__':
-    predict = predict.Predict('data/test15.jpg')
+    predict = predict.Predict('data/test123.jpg')
     labels = list(set(predict.predict()))
     with open('object.txt', 'w') as f:        
         list(map(lambda item : f.write("%s\n" % item), labels))   
