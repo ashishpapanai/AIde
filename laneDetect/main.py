c3d24858e3bb87ebbf51271e821db188b3fe045f
Ashish Papanai <ashishpapanai00@gmail.com>
2021-10-05 06:52:56 +0530

    Added: Replacement image

--- laneDetect/main.py
+++ laneDetect/main.py
@@ -6,12 +6,12 @@ import pyttsx3 as p
 from utils import Util
 
 if __name__ == "__main__":
-    '''video_path = r'E:\road-vision-ai\data\test_vid.mp4' # replace with the full path of video
+    video_path = r'E:\road-vision-ai\data\test_road.mp4' # replace with the full path of video
     lane = Lane(video_path)
-    lane.run(path=video_path)'''
-    image_path = r'E:\road-vision-ai\data\test123.jpg'
+    lane.run(path=video_path)
+    '''image_path = r'E:\road-vision-ai\data\test123.jpg'
     lane = Lane(image_path)
-    lane.run(path=image_path)
+    lane.run(path=image_path)'''
     util = Util()
     util.speak()
 
